84c07e842582b51994b6acd1272081f5c80a8c11
Muhamadrafliseptian <rafliseptiannn25@gmail.com>
2026-05-05 14:50:20 +0700

    push

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,6 +15,19 @@ class UserSeeder extends Seeder
         $menuIds = Menu::pluck('id')->toArray();
         $permissions = implode(',', $menuIds);
 
+        if (!User::where('email', 'admin@local.test')->exists()) {
+            User::create([
+                'name' => 'Super Admin',
+                'email' => 'admin@local.test',
+                'password' => Hash::make('admin123'),
+                'permission' => '',
+                'is_active' => 1,
+                'is_super' => 1,
+                'is_sync' => 1,
+                'role' => 0,
+            ]);
+        }
+
         $employees = Employee::all();
 
         foreach ($employees as $employee) {
@@ -30,7 +43,7 @@ class UserSeeder extends Seeder
                 'id' => $employee->user_id,
                 'name' => $employee->name ?? 'User ' . $employee->user_id,
                 'email' => $email,
-                'password' => Hash::make($employee->user_id), 
+                'password' => Hash::make($employee->user_id),
                 'permission' => $permissions, 
                 'is_active' => 1,
                 'is_super' => 0,
